73bf321ed82978497c3c0e5458f0918a9a11f9e4
Mark Lusby <mlusby@gmail.com>
2013-09-08 20:51:50 -0500

    Introduction to Express tutorial

--- web.js
+++ web.js
@@ -2,8 +2,20 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-	response.send('Hello World! I\'ve added some stuff');	
+var hbs = require('hbs');
+var blogEngine = require('./blog');
+
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
+app.use(express.bodyParser());
+ 
+app.get('/', function(req, res) {
+   res.render('index', {"title":"My Blog", "entries": blogEngine.getBlogEntries()});
+});
+
+app.get('/article/:id', function(req, res) {
+   var entry = blogEngine.getBlogEntry(req.params.id);
+   res.render('article',{title:entry.title, blog:entry});
 });
 
 var port = process.env.PORT || 5000 ;
